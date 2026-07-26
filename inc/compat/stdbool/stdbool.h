d24dc8ab4b3f3c825e630f3f1969d0257ff57514
Mateusz Karcz <mateusz.karcz@interia.eu>
2026-07-27 00:07:23 +0200

    repo: handle missing C99 headers
    - stdbool.h
    - stdint.h

---
+++ inc/compat/stdbool/stdbool.h
@@ -0,0 +1,12 @@
+#ifndef COMPAT_STDBOOL_H
+#define COMPAT_STDBOOL_H
+
+#ifndef __cplusplus
+
+typedef int bool;
+#define false 0
+#define true  1
+
+#endif // __cplusplus
+
+#endif // COMPAT_STDBOOL_H
